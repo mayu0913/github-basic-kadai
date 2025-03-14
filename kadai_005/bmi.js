d5e898c2dd3afc5b2cf63e9dafd2de2db47e188f
mayu0913 <mayu.yamada913@gmail.com>
2025-03-14 23:57:47 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,7 +1,11 @@
 let bodyWeight;
 let bodyHeight;
 
-bodyWeight = '68';
-bodyHeight = '1.7';
+bodyWeight = 68;
+bodyHeight = 1.7;
+
+console.log(bodyWeight);
+console.log(bodyHeight);
+
 
 console.log([bodyWeight] / [bodyHeight*bodyHeight]);
